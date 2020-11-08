e865c4c7cfee93ecf794e2ae796d4b22bfdec558
Jay DeLuca <jaydeluca4@gmail.com>
2020-11-08 14:09:34 -0500

    Add elasticsearch charts

---
+++ terraform/modules/elasticsearch/variables.tf
@@ -0,0 +1,17 @@
+variable "namespace" {}
+
+variable "environment_name" {}
+
+variable "limit_cpu" {}
+
+variable "limit_memory" {}
+
+variable "requests_cpu" {}
+
+variable "requests_memory" {}
+
+variable "storage_class_name" {}
+
+variable "volume_storage" {
+    default = "30Gi"
+}
